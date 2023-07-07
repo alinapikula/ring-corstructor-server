aac0519b3415271aca958985668e0023e29f8b51
Alina <allianpi@gmail.com>
2023-07-06 20:00:03 -0400

    knex setup

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
-
+const mysql = require('mysql');
+const path = require('path');
 
 app.listen(8080, () => {
     console.log('Server Started on http://localhost:8080');
