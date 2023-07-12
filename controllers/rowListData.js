fe7733ec56059719b3551b7c9af279b350d8ad1d
Alina <allianpi@gmail.com>
2023-07-11 22:08:07 -0400

    retrive individual row position objects

--- controllers/rowListData.js
+++ controllers/rowListData.js
@@ -11,6 +11,80 @@ const rowList = (req,res) =>{
     })
 }
 
+
+const rowSelected1 = (req, res) =>{
+knex ("pos1")
+.where({'pos1.id' :req.params.id})
+.then((result) =>{
+        if (result.length === 0) {
+                res.status(404)
+                .json(`No item found with id ${req.params.id}`);
+            }
+            res.status(200).json(result[0]);
+})
+.catch((err) =>
+            res.status(400).send(`Error: ${err}`)
+        );
+}
+
+const rowSelected2 = (req, res) =>{
+knex ("pos2")
+.where({'pos2.id' :req.params.id})
+.then((result) =>{
+        if (result.length === 0) {
+                res.status(404)
+                .json(`No item found with id ${req.params.id}`);
+            }
+            res.status(200).json(result[0]);
+})
+.catch((err) =>
+            res.status(400).send(`Error: ${err}`)
+        );
+}
+
+
+const rowSelected3 = (req, res) =>{
+knex ("pos3")
+.where({'pos3.id' :req.params.id})
+.then((result) =>{
+    if (result.length === 0) {
+                res.status(404)
+            .json(`No item found with id ${req.params.id}`);
+            }
+            res.status(200).json(result[0]);
+})
+.catch((err) =>
+            res.status(400).send(`Error: ${err}`)
+        );
+}
+
+const rowSelected12 = (req, res) =>{
+knex ("row_list")
+.join('pos1', 'row_list.pos1', '=', 'pos1.img')
+
+
+
+// .select("id", "img")
+// .join('pos2', 'row_list.pos2', '=', 'pos2.img')
+// .select("row_list.id","row_list.pos1","row_list.pos1")
+.where({'row_list.row_id' :req.params.id} )
+.then((result) =>{
+    if (result.length === 0) {
+                res.status(404)
+            .json(`No item found with id ${req.params.id}`);
+            }
+            res.status(200).json(result[0]);
+})
+.catch((err) =>
+            res.status(400).send(`Error: ${err}`)
+        );
+}
+
+
 module.exports ={
-    rowList
+    rowList,
+    rowSelected1,
+    rowSelected2,
+    rowSelected3,
+    rowSelected12
 }
--- routes/rowList.js
+++ routes/rowList.js
@@ -5,5 +5,18 @@ const rowListController = require('../controllers/rowListData.js')
 router.route('/row-list')
   .get(rowListController.rowList)
 
+router.route('/pos1/:id')
+  .get(rowListController.rowSelected1)
+
+router.route('/pos2/:id')
+  .get(rowListController.rowSelected2)
+
+router.route('/pos3/:id')
+  .get(rowListController.rowSelected3)
+
+router.route('/pos1-2/:id1')
+  .get(rowListController.rowSelected12)
+
+
 
 module.exports = router;
